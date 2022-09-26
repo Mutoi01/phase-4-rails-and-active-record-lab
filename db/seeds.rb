6f1795a79514332f6a42446b3cc396f940878ba1
Mutoi01 <pngando9@gmail.com>
2022-09-26 08:28:48 +0300

    commit tree 77dbc1774ee0ef51845b12a78204fb03af25a7e9

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Student.create!(first_name: "the", last_name: "whispers", grade: 4.0)
